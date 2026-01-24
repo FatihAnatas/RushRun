7518323e247fc5032f1f8a580f4877f46cc1ce17
Fatih <fatihsametanatas@gmail.com>
2026-01-24 20:24:47 +0300

    RushRunYedek1

---
+++ RushRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Ayarlar")]
+    public float jumpForce = 10f; 
+    public LayerMask groundLayer; 
+    public Transform groundCheck; 
+    public float checkRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        if (isGrounded && (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = Vector2.up * jumpForce;
+    }
+}
